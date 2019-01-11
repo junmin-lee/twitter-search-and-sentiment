fdc52b61ea81eea72220de007d44466bb1ba2be1
junmin-lee <jm83.lee@gmail.com>
2019-01-11 16:49:15 +0900

    Change to csv output with positive negative label

--- 110119_LPDS_getting_tweets.py
+++ 110119_LPDS_getting_tweets.py
@@ -1,13 +1,13 @@
 
-#Don't forget to restart the termial when after setting environment varaibles
-
 import os
 import tweepy
 from textblob import TextBlob
+import csv
 
+#Twitter Key and token configuration
+#Don't forget to restart the termial when after setting environment variables
 consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_key_secret = os.environ.get('CONSUMER_KEY_SECRET')
-
 access_token = os.environ.get('ACCESS_TOKEN')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
@@ -21,8 +21,22 @@ api = tweepy.API(auth)
 #Search
 public_tweets = api.search('Trump')
 
-#Print the tweets and get the sentiments
+# Prepare to write csv file as an output
+tweet_file = open('tweet_output.csv','w',encoding='utf-8',newline='')
+
 for tweet in public_tweets:
-    print(tweet.text)
+    #tweets to go in the first colomn
+    tweet_content = tweet.text
+    #positive/negative label for second colomn, score calculation -1~1
     analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
+    tweet_score = analysis.sentiment.polarity * analysis.sentiment.subjectivity
+    label = ''
+    if tweet_score < 0:
+        label = 'Negative'
+    else:
+        label = 'Positive'
+    #Print the tweets and get the sentiments
+    write_row = csv.writer(tweet_file)
+    write_row.writerow([tweet_content, label])
+
+tweet_file.close()
